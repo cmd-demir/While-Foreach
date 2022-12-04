342b7310cb1324ee3bcaa2af389589ab32aca25b
Cem Demir <cmd1220@gmail.com>
2022-12-04 17:24:21 +0300

    hepsi eklendi

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //While
+        //1 den başlayarak console dan girilen sayıya kadar(sayı dahil) ortalama hesaplayığ console a yazdıran program
+        Console.Write("Lütfen bir sayı giriniz:");
+        int sayi = int.Parse(Console.ReadLine());
+        int sayac = 1;
+        int toplam = 0;
+        while(sayac<=sayi)
+        {
+            toplam += sayac;
+            sayac ++;
+        }
+        Console.WriteLine(toplam/sayi);
+         // a'dan z'ye kadar tüm harfleri console'a yazdır
+       char character = 'a';
+       
+       while (character<'z')
+       {
+        Console.WriteLine(character);
+        character ++;
+       }
+    }
+       
+}
